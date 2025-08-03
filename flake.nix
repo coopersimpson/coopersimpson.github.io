ac5458b371949548f18747a224fcfa6154e57987
Cooper Simpson <cooper@coopersimpson.com>
2025-08-03 15:29:22 +1000

    Use nix dev environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Ruby + Bundler developer environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "ruby-dev-shell";
+
+          buildInputs = [
+            pkgs.ruby
+            pkgs.bundler
+          ];
+
+          shellHook = ''
+            echo "Ruby: $(ruby -v)"
+            echo "Bundler: $(bundler -v)"
+          '';
+        };
+      });
+}
